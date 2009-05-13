35e57f7ae1d80aac024a80f867d2610ec93ed9f3
Horst Gutmann <zerok@zerokspot.com>
2009-05-13 15:13:15 +0200

    The test-environment has now two testrunners: test-1.0 and test-1.1

---
+++ examples/blogserver/api/handlers.py
@@ -0,0 +1,32 @@
+from piston.handler import BaseHandler, AnonymousBaseHandler
+from piston.utils import rc, require_mime, require_extended
+
+from blogserver.blog.models import Blogpost
+
+class AnonymousBlogpostHandler(AnonymousBaseHandler):
+    model = Blogpost
+    fields = ('title', 'content', 'created_on')
+
+class BlogpostHandler(BaseHandler):
+    model = Blogpost
+    anonymous = AnonymousBlogpostHandler
+    fields = ('title', 'content', ('author', ('username',)), 
+              'created_on', 'content_length')
+    
+    def content_length(self, blogpost):
+        return len(blogpost.content)
+        
+    @require_extended
+    def create(self, request):
+        attrs = self.flatten_dict(request.POST)
+
+        if self.exists(**attrs):
+            return rc.DUPLICATE_ENTRY
+        else:
+            post = Blogpost(title=attrs['title'], 
+                            content=attrs['content'],
+                            author=request.user)
+            post.save()
+            
+            return post
+        
